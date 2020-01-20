7a1cfdde53bd8f40502e624ea268222f298aaacd
Magalhaes Silva <slml@gft.com>
2020-01-20 17:24:17 -0200

    CPF Validation complete

--- force-app/main/default/aura/CustomCasePage/CustomCasePageController.js
+++ force-app/main/default/aura/CustomCasePage/CustomCasePageController.js
@@ -1,21 +1,9 @@
 ({
-    handleLoad: function(cmp, event, helper) {
-        cmp.set('v.showSpinner', false);
+    doInit: function(cmp, event, helper) {
+     
     },
 
-    handleSubmit: function(cmp, event, helper) {
-        cmp.set('v.disabled', true);
-        cmp.set('v.showSpinner', true);
-    },
-
-    handleError: function(cmp, event, helper) {
-        // errors are handled by lightning:inputField and lightning:nessages
-        // so this just hides the spinnet
-        cmp.set('v.showSpinner', false);
-    },
-
-    handleSuccess: function(cmp, event, helper) {
-        cmp.set('v.showSpinner', false);
-        cmp.set('v.saved', true);
-    }
+    handleCpf: function(cmp, event, helper) {      
+       helper.handleCpf(cmp,event, helper);
+    }     
 })
--- force-app/main/default/aura/CustomCasePage/CustomCasePageHelper.js
+++ force-app/main/default/aura/CustomCasePage/CustomCasePageHelper.js
@@ -1,5 +1,50 @@
-({
-    helperMethod : function() {
+({    
+    handleCpf: function(cmp, event, helper) {      
+        let cpf = cmp.find("auIdCpf").get("v.value");           
+        let validRe = new RegExp("([0-9]{11})"); 
+        let invalidRe = new RegExp("(0{9}|1{9}|2{9}|3{9}|4{9}|5{9}|6{9}|7{9}|8{9}|9{9})");
+        let isValid = false;   
+        console.log("validRe = " + validRe.test(cpf));
+        console.log("InvalidRe =" + invalidRe.test(cpf));
 
+        if(validRe.test(cpf) && !invalidRe.test(cpf)) {
+            var maskedCpf = cpf.substring(0,3) + '.' + cpf.substring(3,6) +'.'+ cpf.substring(6,9)+'-'+ cpf.substring(9,11);     
+            cmp.set("v.varCpf", maskedCpf);               
+            isValid = this.validCpf(cmp, event, helper,cpf); 
+            console.log("CPF VALIDO AEEEEEEEEEEEEEEEEEEEEE");         
+        } else {
+            console.log("CPF INVALIDO");
+        }
+    },  
+
+    validCpf: function(cmp, event, helper, cpf) {              
+        let soma=0;
+        let resto=0;
+        let c=1;
+        let dig=0;
+
+
+        for(var i=10; i>=2;i--){       
+            soma+= parseInt(cpf.substring(c-1,c) * i ); 
+            c++;               
+        }
+
+        dig = parseInt(cpf.substring(9,10));
+        resto = (soma*10)%11;  
+        if(resto==dig){
+            c=1;
+            soma =0 ;
+            for(i=11; i>=2;i--){       
+                soma+= parseInt(cpf.substring(c-1,c) * i ); 
+                c++;               
+            }       
+            resto = (soma*10)%11;
+            dig = parseInt(cpf.substring(10,11));          
+            if(resto ==dig)
+                return true;
+            else 
+                return false;             
+        }        
+        return false;       
     }
 })
